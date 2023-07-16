5788fda53dd8068d5ef3c58dd1226947e52f6bd6
vnanhkhoa <anhkhoadn20@gmail.com>
2023-07-16 13:25:03 +0700

    Add new project

---
+++ app/src/main/java/com/khoavna/asteroidradar/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.khoavna.asteroidradar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.squareup.picasso.BuildConfig
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/khoavna/asteroidradar/data/repositores/shoe/ShoeRepositoryIml.kt
@@ -0,0 +1,25 @@
+package com.khoavna.shoestore.data.repositores.shoe
+
+import com.khoavna.shoestore.data.datasource.shoe.ShoeDataSource
+import com.khoavna.asteroidradar.data.model.Shoe
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
+class ShoeRepositoryIml(private val shoeDataSource: ShoeDataSource) : ShoeRepository {
+
+    override fun getShoes(): StateFlow<List<Shoe>> = shoeDataSource.findAll()
+
+    override fun getShoe(id: Int): Flow<Shoe?> = shoeDataSource.findOne(id = id)
+
+    override fun delete(shoe: Shoe) {
+        shoeDataSource.delete(shoe)
+    }
+
+    override fun insert(shoe: Shoe) {
+        shoeDataSource.insert(shoe)
+    }
+
+    override fun update(shoe: Shoe) {
+        shoeDataSource.update(shoe)
+    }
+}
